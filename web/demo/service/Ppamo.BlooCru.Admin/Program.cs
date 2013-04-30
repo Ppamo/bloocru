c19012906d0f4dfd618bd43ccf810acf15097691
Pablo <pablo@ppamo.cl>
2013-04-30 18:14:54 +0000

    First version
    git-svn-id: http://localhost/svn/project.bloocru@61 ba9d1529-2e74-4a06-b9c6-6400ac57aca1

---
+++ web/demo/service/Ppamo.BlooCru.Admin/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Ppamo.BlooCru.Admin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ web/demo/service/Ppamo.BlooCru.WS.Data/MySQL/structure.sql
@@ -0,0 +1,171 @@
+SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
+SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
+SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL';
+
+-- -----------------------------------------------------
+-- Table `user`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `user` ;
+CREATE  TABLE IF NOT EXISTS `user`
+(
+	`id` INT(11) NOT NULL AUTO_INCREMENT ,
+	`login` VARCHAR(45) NOT NULL ,
+	`email` CHAR(45) NOT NULL ,
+	`pass` CHAR(30) NOT NULL ,
+	PRIMARY KEY (`id`) ,
+	INDEX `IN_UserByLogin` (`login` ASC)
+)
+ENGINE = MyISAM
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = latin1;
+
+-- -----------------------------------------------------
+-- Table `place`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `place` ;
+CREATE  TABLE IF NOT EXISTS `place`
+(
+	`id` INT(11) NOT NULL AUTO_INCREMENT ,
+	`name` VARCHAR(45) NOT NULL ,
+	`latitude` INT,
+	`longitude` INT,
+	`zoom` INT,
+	PRIMARY KEY (`id`) ),
+	INDEX `IN_PlaceByName` (`name` ASC)
+)
+ENGINE = MyISAM
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = latin1;
+
+-- -----------------------------------------------------
+-- Table `mark`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `mark` ;
+CREATE  TABLE IF NOT EXISTS `mark`
+(
+	`id` INT(11) NOT NULL AUTO_INCREMENT ,
+	`title` VARCHAR(45) NOT NULL ,
+	`placeId` INT NOT NULL,
+	PRIMARY KEY (`id`),
+	CONSTRAINT `FK_MarkAtPlace`
+		FOREIGN KEY (`placeId`)
+		REFERENCES `place` (`id`)
+		ON DELETE RESTRICT
+		ON UPDATE NO ACTION,
+	INDEX `IN_MarkByTitle` (`title` ASC)
+)
+ENGINE = MyISAM
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = latin1;
+
+-- -----------------------------------------------------
+-- Table `userPropertyName`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `userPropertyName` ;
+CREATE  TABLE IF NOT EXISTS `userPropertyName`
+(
+	`id` INT NOT NULL AUTO_INCREMENT ,
+	`name` VARCHAR(45) NOT NULL,
+	PRIMARY KEY (`id`) ),
+	INDEX `IN_UserPropertyNameByName` (`name` ASC)
+)
+ENGINE = MyISAM
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = latin1;
+
+-- -----------------------------------------------------
+-- Table `userPropertyValue`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `userPropertyValue` ;
+CREATE  TABLE IF NOT EXISTS `userPropertyValue`
+(
+	`id` INT NOT NULL AUTO_INCREMENT ,
+	`value` VARCHAR(300) NOT NULL,
+	PRIMARY KEY (`id`),
+	INDEX `IN_UserPropertyValueByValue` (`value` ASC)
+)
+ENGINE = MyISAM
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = latin1;
+
+-- -----------------------------------------------------
+-- Table `userProperty`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `userProperty` ;
+CREATE  TABLE IF NOT EXISTS `userProperty`
+(
+	`userId` INT(11) NOT NULL,
+	`userPropertyNameId` INT NOT NULL,
+	`userPropertyValueId` INT NOT NULL,
+	PRIMARY KEY (`id`),
+	CONSTRAINT `FK_UserPropertyName`
+		FOREIGN KEY (`userPropertyNameId` )
+		REFERENCES `userPropertyName` (`id` )
+		ON DELETE RESTRICT
+		ON UPDATE NO ACTION,
+	CONSTRAINT `FK_UserPropertyValue`
+		FOREIGN KEY (`userPropertyValueId` )
+		REFERENCES `userPropertyValue` (`id` )
+		ON DELETE RESTRICT
+		ON UPDATE NO ACTION
+)
+ENGINE = MyISAM
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = latin1;
+
+-- -----------------------------------------------------
+-- Table `notice`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `notice` ;
+CREATE  TABLE IF NOT EXISTS `notice`
+(
+	`id` INT(11) NOT NULL AUTO_INCREMENT ,
+	`title` VARCHAR(45) NOT NULL,
+	`description` VARCHAR(300) NOT NULL,
+	`timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+	`userId` INT NOT NULL,
+	PRIMARY KEY (`id`) ,
+	INDEX `IN_NoticeByTitle` (`title` ASC),
+	INDEX `IN_NoticeByTimestamp` (`timestamp` DESC),
+	CONSTRAINT `FK_NoticeFromId`
+		FOREIGN KEY (`userId`)
+		REFERENCES `user` (`id`)
+		ON DELETE RESTRICT
+		ON UPDATE NO ACTION
+)
+ENGINE = MyISAM
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = latin1;
+
+-- -----------------------------------------------------
+-- Table `event`
+-- -----------------------------------------------------
+DROP TABLE IF EXISTS `event` ;
+CREATE  TABLE IF NOT EXISTS `event`
+(
+	`id` INT(11) NOT NULL AUTO_INCREMENT,
+	`description` VARCHAR(300) NOT NULL,
+	`timestamp` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP ,
+	`userId` INT NOT NULL,
+	`markId` INT NOT NULL,
+	PRIMARY KEY (`id`) ,
+	INDEX `IN_NoticeByTitle` (`title` ASC),
+	INDEX `IN_NoticeByTimestamp` (`timestamp` DESC),
+	CONSTRAINT `FK_EventFromUser`
+		FOREIGN KEY (`userId`)
+		REFERENCES `user` (`id`)
+		ON DELETE RESTRICT
+		ON UPDATE NO ACTION,
+	CONSTRAINT `FK_EventAtMark`
+		FOREIGN KEY (`markId`)
+		REFERENCES `mark` (`id`)
+		ON DELETE RESTRICT
+		ON UPDATE NO ACTION
+)
+ENGINE = MyISAM
+AUTO_INCREMENT = 1
+DEFAULT CHARACTER SET = latin1;
+
+SET SQL_MODE=@OLD_SQL_MODE;
+SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS;
+SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS;
