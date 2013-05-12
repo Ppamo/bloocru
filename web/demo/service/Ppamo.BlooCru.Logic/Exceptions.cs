333a7a1a04cef3ad41d5c0627950003a20838e5e
Pablo <pablo@ppamo.cl>
2013-05-12 01:38:57 +0000

    First version of the service rules with new Ppamo.Restful design
    git-svn-id: http://localhost/svn/project.bloocru@73 ba9d1529-2e74-4a06-b9c6-6400ac57aca1

--- web/demo/service/Ppamo.BlooCru.Logic/Exceptions.cs
+++ web/demo/service/Ppamo.BlooCru.Logic/Exceptions.cs
@@ -9,7 +9,7 @@ namespace Ppamo.BlooCru.Logic
     #region "Exceptions"
 
     #region "NonValidKeyException"
-    public class NonValidKeyException : Exception
+    public class NonValidKeyException : Ppamo.Common.DataProvider.DataBase.cboException
     {
 
         #region "Construtor"
@@ -17,6 +17,35 @@ namespace Ppamo.BlooCru.Logic
         public NonValidKeyException()
         {
             this.__message = "The navigation key is not valid";
+        }
+
+        #endregion
+        #region "Message"
+        private string __message;
+        public override string Message
+        {
+            get
+            {
+                return __message;
+            }
+        }
+        #endregion
+
+    }
+    #endregion
+    #region "InvalidURIException"
+    public class InvalidURIException : Ppamo.Common.DataProvider.DataBase.cboException
+    {
+
+        #region "Construtor"
+
+        public InvalidURIException()
+        {
+            this.__message = "Invalid uri";
+        }
+        public InvalidURIException(string uri)
+        {
+            this.__message = "Invalid uri '" + uri.Trim() + "'";
         }
 
         #endregion
--- web/demo/service/Ppamo.BlooCru.Logic/Rules.cs
+++ web/demo/service/Ppamo.BlooCru.Logic/Rules.cs
@@ -2,47 +2,117 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using Ppamo.Common.RESTFulServer;
-using Ppamo.Common.RESTFulServer.Data;
 using Ppamo.BlooCru.WS.Data.CBO;
+using Ppamo.RESTFulWebServer;
+using Ppamo.RESTFulWebServer.Data;
 
 namespace Ppamo.BlooCru.Logic
 {
 
-    public class Rules : IServiceHandler
+    public class Rules : IServerHandler
     {
 
         #region "Delegates"
 
-        #region "getNavigationkey"
+        #region "getNavigationKey"
 
         public RESTFulResponse getNavigationKey(RESTFulQuery query)
         {
-            // GET /nav/{login}/{code}
-            string email = query.queryNodes[1];
-            string code = query.queryNodes[2];
-
-            return new RESTFulResponse("AAA");
+            // POST /key/
+            return new RESTFulResponse(new InvalidURIException(query.queryPath));
         }
 
         #endregion
-        #region "getPeopleById"
-
-        public RESTFulResponse getPeopleById(RESTFulQuery query)
+        #region "localize"
+        public RESTFulResponse localize(RESTFulQuery query)
         {
-            // GET /{key}/people/(id)
-            if (checkKey(query))
-            {
-                userCBO user = null;
-                user.id = int.Parse(query.queryNodes[2]);
-                Worker.DbProvider.load(user);
-
-            }
-            else
-            {
-                return new RESTFulResponse(new  NonValidKeyException());
-            }
-            return new RESTFulResponse("AAA");
+            // GET /{key}/localize : placeCBO
+            
+            return new RESTFulResponse(new InvalidURIException(query.queryPath));
+        }
+        #endregion
+        #region "getProfile"
+        public RESTFulResponse getProfile(RESTFulQuery query)
+        {
+            // GET /{key}/profile/(id) : userCBO
+            return new RESTFulResponse(new InvalidURIException(query.queryPath));
+        }
+        #endregion
+        #region "listPlaces"
+        public RESTFulResponse listPlaces(RESTFulQuery query)
+        {
+            // GET /{key}/place : placeCBOCollection
+            return new RESTFulResponse(new InvalidURIException(query.queryPath));
+        }
+        #endregion
+        #region "listActivitiesByPlace"
+        public RESTFulResponse listActivitiesByPlace(RESTFulQuery query)
+        {
+            // GET /{key}/place/(id)/activity : placeCBOCollection
+            return new RESTFulResponse(new InvalidURIException(query.queryPath));
+        }
+        #endregion
+        #region "getActivity"
+        public RESTFulResponse getActivity(RESTFulQuery query)
+        {
+            // GET /{key}/activity/(id) : activityCBO
+            return new RESTFulResponse(new InvalidURIException(query.queryPath));
+        }
+        #endregion
+        #region "listParticipantsInActivity"
+        public RESTFulResponse listParticipantsInActivity(RESTFulQuery query)
+        {
+            // GET /{key}/activity/(id)/people : peopleCBOCollection
+            return new RESTFulResponse(new InvalidURIException(query.queryPath));
+        }
+        #endregion
+        #region "storeParticipant"
+        public RESTFulResponse storeParticipant(RESTFulQuery query)
+        {
+            // POST /{key}/activity/(id)/people : peopleCBO
+            return new RESTFulResponse(new InvalidURIException(query.queryPath));
+        }
+        #endregion
+        #region "storeActivity"
+        public RESTFulResponse storeActivity(RESTFulQuery query)
+        {
+            // POST /{key}/activity : activityCBO
+            return new RESTFulResponse(new InvalidURIException(query.queryPath));
+        }
+        #endregion
+        #region "listEventsByPlace"
+        public RESTFulResponse listEventsByPlace(RESTFulQuery query)
+        {
+            // GET /{key}/city/(id)/event : eventCBOCollection
+            return new RESTFulResponse(new InvalidURIException(query.queryPath));
+        }
+        #endregion
+        #region "getEvent"
+        public RESTFulResponse getEvent(RESTFulQuery query)
+        {
+            // GET /{key}/event/(id) : eventCBO
+            return new RESTFulResponse(new InvalidURIException(query.queryPath));
+        }
+        #endregion
+        #region "storePlace"
+        public RESTFulResponse storePlace(RESTFulQuery query)
+        {
+            // POST /{key}/place : placeCBO
+            return new RESTFulResponse(new InvalidURIException(query.queryPath));
+        }
+        #endregion
+        #region "storeEvent"
+        public RESTFulResponse storeEvent(RESTFulQuery query)
+        {
+            // POST /{key}/event : eventCBO
+            return new RESTFulResponse(new InvalidURIException(query.queryPath));
+        }
+        #endregion
+        #region "defaultRule"
+        public RESTFulResponse defaultRule(RESTFulQuery query)
+        {
+            // *
+            return new RESTFulResponse(new InvalidURIException(query.queryPath));
         }
         #endregion
 
@@ -58,55 +128,29 @@ namespace Ppamo.BlooCru.Logic
         }
 
         #endregion
-        #region "SerializeJsonResponse"
-
-        private string SerializeJsonResponse(string code)
-        {
-
-            return string.Empty;
-        }
 
         #endregion
-
-        #endregion
-
         #region "Populate Behaviors"
-        public RESTFulBehavior[] getBehaviors()
+        public RESTFulBehavior[] GetBehaviors()
         {
-            RESTFulBehavior[] list = (RESTFulBehavior[])Array.CreateInstance(typeof(RESTFulBehavior), 15);
 
-            // GET /nav/{login}/{code} <-- {key}
-            list[0] = new RESTFulBehavior("^GET /nav/\\{.*\\}[/]?$", getNavigationKey);
-            // GET /{key}/people/(id)
-            list[1] = new RESTFulBehavior("^GET /\\{.*\\}/\\{.*\\}/people/[0-9][0-9]*$", getPeopleById);
-            // GET /{key}/localizate
-            list[2] = new RESTFulBehavior("", null);
-            // GET /{key}/city
-            list[3] = new RESTFulBehavior("", null);
-            // GET /{key}/city/(id)
-            list[4] = new RESTFulBehavior("", null);
-            // GET /{key}/city/(id)/people
-            list[5] = new RESTFulBehavior("", null);
-            // GET /{key}/city/(id)/activity
-            list[6] = new RESTFulBehavior("", null);
-            // GET /{key}/city/(id)/activity/(id)
-            list[7] = new RESTFulBehavior("", null);
-            // GET /{key}/city/(id)/activity/(id)/participate
-            list[8] = new RESTFulBehavior("", null);
-            // GET /{key}/city/(id)/activity/(id)/participate/(id)
-            list[9] = new RESTFulBehavior("", null);
-            // GET /{key}/city/(id)/event
-            list[10] = new RESTFulBehavior("", null);
-            // GET /{key}/city/(id)/event/(id)
-            list[11] = new RESTFulBehavior("", null);
-            // GET /{key}/place/(id)
-            list[12] = new RESTFulBehavior("", null);
-            // POST /{key}/city/(id)/activity
-            list[13] = new RESTFulBehavior("", null);
-            // POST /{key}/event
-            list[14] = new RESTFulBehavior("", null);
+            List<RESTFulBehavior> list = new List<RESTFulBehavior>();
+            list.Add(new RESTFulBehavior("^POST /key[/]?$", getNavigationKey));
+            list.Add(new RESTFulBehavior("^GET /\\{.*\\}/localize[/]?$", localize));
+            list.Add(new RESTFulBehavior("^GET /\\{.*\\}/profile/[0-9]+$", getProfile));
+            list.Add(new RESTFulBehavior("^GET /\\{.*\\}/place[/]?$", listPlaces));
+            list.Add(new RESTFulBehavior("^GET /\\{.*\\}/place/[0-9]+/activity[/]?$", listActivitiesByPlace));
+            list.Add(new RESTFulBehavior("^GET /\\{.*\\}/activity/[0-9]+$", getActivity));
+            list.Add(new RESTFulBehavior("^GET /\\{.*\\}/activity/[0-9]+/people[/]?$", listParticipantsInActivity));
+            list.Add(new RESTFulBehavior("^POST /\\{.*\\}/activity/[0-9]+/people[/]?$", storeParticipant));
+            list.Add(new RESTFulBehavior("^POST /\\{.*\\}/activity[/]?$", storeActivity));
+            list.Add(new RESTFulBehavior("^GET /\\{.*\\}/place/[0-9]+/event[/]?$", listEventsByPlace));
+            list.Add(new RESTFulBehavior("^GET /\\{.*\\}/event/[0-9]+$", getEvent));
+            list.Add(new RESTFulBehavior("^POST /\\{.*\\}/place[/]?$", storePlace));
+            list.Add(new RESTFulBehavior("^POST /\\{.*\\}/event[/]?$", storeEvent));
+            list.Add(new RESTFulBehavior("*", defaultRule));
+            return list.ToArray();
 
-            return list;
         }
         #endregion
 
