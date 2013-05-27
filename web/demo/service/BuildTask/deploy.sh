f06dd73b1d3553036ecf6e9c0867299f0a262f64
Pablo <pablo@ppamo.cl>
2013-05-27 08:36:27 +0000

    add deploy build task
    git-svn-id: http://localhost/svn/project.bloocru@99 ba9d1529-2e74-4a06-b9c6-6400ac57aca1

---
+++ web/demo/service/BuildTask/deploy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mono Utils/nant.win-0.92/bin/NAnt.exe -D:deployFile="/home/development/svn/TopicSearcher/Deploy/Ppamo.BlooCru.zip" -buildfile:build.nant "deploy"
+
+mono Utils/nant.win-0.92/bin/NAnt.exe -buildfile:build.nant "setup"
+
