bf6e1da614fca491edaea5abd87b9c9eb9754693
Build <info@ppamo.cl>
2013-05-29 14:35:41 +0000

    deploying into bloocru
    git-svn-id: http://localhost/svn/project.bloocru@113 ba9d1529-2e74-4a06-b9c6-6400ac57aca1

--- web/demo/service/BuildTask/deploy.bloocru.sh
+++ web/demo/service/BuildTask/deploy.bloocru.sh
@@ -3,5 +3,5 @@
 
 # GET A FRESH VERSION
 
-mono Utils/nant.win-0.92/bin/NAnt.exe -D:deployFile="../Ppamo.BlooCru.zip" -buildfile:build.nant "deploy.setup"
+nant.sh -D:deployFile="../Ppamo.BlooCru.zip" -buildfile:build.nant "deploy.setup.bloocru"
 
