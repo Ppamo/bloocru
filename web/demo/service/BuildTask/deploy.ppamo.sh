4097b13609207d3c67b32ae91d09d2cff834e1d1
Pablo <pablo@ppamo.cl>
2013-05-29 07:42:01 +0000

    add deploy for bloocru production
    git-svn-id: http://localhost/svn/project.bloocru@112 ba9d1529-2e74-4a06-b9c6-6400ac57aca1

--- web/demo/service/BuildTask/deploy.ppamo.sh
+++ web/demo/service/BuildTask/deploy.ppamo.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mono Utils/nant.win-0.92/bin/NAnt.exe -D:deployFile="/home/development/svn/TopicSearcher/Deploy/Ppamo.BlooCru.zip" -buildfile:build.nant "deploy.setup"
+mono Utils/nant.win-0.92/bin/NAnt.exe -D:deployFile="/home/development/svn/TopicSearcher/Deploy/Ppamo.BlooCru.zip" -buildfile:build.nant "deploy.setup.ppamo"
 
