02056e1ebfc13ac7a4033857a2d6cee1d71740fa
Pablo <pablo@ppamo.cl>
2013-05-29 00:21:08 +0000

    change the deploy task to make a diferent deploy.bloocru task
    git-svn-id: http://localhost/svn/project.bloocru@109 ba9d1529-2e74-4a06-b9c6-6400ac57aca1

--- web/demo/service/BuildTask/deploy.sh
+++ web/demo/service/BuildTask/deploy.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-mono Utils/nant.win-0.92/bin/NAnt.exe -D:deployFile="/home/development/svn/TopicSearcher/Deploy/Ppamo.BlooCru.zip" -buildfile:build.nant "deploy"
-mono Utils/nant.win-0.92/bin/NAnt.exe -buildfile:build.nant "setup"
+mono Utils/nant.win-0.92/bin/NAnt.exe -D:deployFile="/home/development/svn/TopicSearcher/Deploy/Ppamo.BlooCru.zip" -buildfile:build.nant "deploy.setup"
 
