cade624abb88e9dc76c134ae29bfe640b40fc19c
Build <info@ppamo.cl>
2013-05-30 02:21:55 +0000

    complete the deploy build in bloocru production
    git-svn-id: http://localhost/svn/project.bloocru@118 ba9d1529-2e74-4a06-b9c6-6400ac57aca1

--- web/demo/service/BuildTask/deploy.bloocru.sh
+++ web/demo/service/BuildTask/deploy.bloocru.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-nant.sh -D:deployFile="../Ppamo.BlooCru.zip" -buildfile:build.nant "deploy.setup.bloocru"
+nant -D:deployFile="../Ppamo.BlooCru.zip" -buildfile:build.nant "deploy.setup.bloocru"
 
